885170ddd1524340d4b7600ad3435ebd0d9dc77a
xianglw2h <143702662+xianglw2h@users.noreply.github.com>
2025-05-14 12:17:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ friendly_names = {
 #############################
 # 1. 页面设置与自定义 CSS
 #############################
-st.set_page_config(page_title="记住我的名字：梁翔(⌐■_■)", layout="wide")
+st.set_page_config(page_title="欢迎使用软件(⌐■_■)", layout="wide")
 # ...existing code...
 st.markdown(
     """
